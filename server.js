989476bbbc9968e31447f48b978bea2675706f68
Lyndsey Padget <lpadget@longplaysoftware.com>
2019-02-17 13:03:51 -0600

    Get route?

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ var express = require('express');
 var app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, '_book')));
+
+app.use('/', express.static(path.join(__dirname, '_book')))
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
 app.listen(process.env.PORT || 4000);
