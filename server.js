24c069a0772f9e516dfdc4a7d6cc10813db9273c
Lyndsey Padget <lpadget@longplaysoftware.com>
2019-02-17 13:10:09 -0600

    Simplify

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, '_book')));
 
 app.use('/', express.static(path.join(__dirname, '_book')))
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
 
 app.listen(process.env.PORT || 4000);
